ac0f37078d663ee38ed3c809feed3d10d848a90b
‘Evan <puzonevan@gmail.com>
2022-04-19 15:24:24 -0700

    perl json

--- cgi-bin/perl/perl-hello-json.pl
+++ cgi-bin/perl/perl-hello-json.pl
@@ -10,5 +10,5 @@ $address = $ENV{REMOTE_ADDR};
 
 my %message = ('title' => 'Hello, Perl!', 'heading' => 'Hello, Perl!', 'message' => 'This page was generated with the Perl programming language', 'time' => $date, 'IP' => $address);
 
-# my $json = encode_json \%message;
-# print "$json\n";
+my $json = encode_json \%message;
+print "$json\n";
