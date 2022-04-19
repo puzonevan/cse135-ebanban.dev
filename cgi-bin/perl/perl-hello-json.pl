cfc1b8ab883ccd70529683607ccf2229e357c498
‘Evan <puzonevan@gmail.com>
2022-04-19 15:21:02 -0700

    perl json

--- cgi-bin/perl/perl-hello-json.pl
+++ cgi-bin/perl/perl-hello-json.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 # In Perl, you must first install the JSON package from CPAN (the Perl equivalent to npm)
-# use JSON;
+use JSON;
 
 print "Cache-Control: no-cache\n";
 print "Content-type: application/json\n\n";
