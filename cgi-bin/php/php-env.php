096c894dc33a37903f538bfc768fca65b7adf75c
‘Evan <puzonevan@gmail.com>
2022-04-20 14:40:22 -0700

    php env

--- cgi-bin/php/php-env.php
+++ cgi-bin/php/php-env.php
@@ -13,7 +13,7 @@
     <body>
         <h1 align="center">Environment Variables</h1>
         <?php
-            $env = getenv();
+            $env = $_SERVER;
 
             
             foreach($env as $key => $value){
