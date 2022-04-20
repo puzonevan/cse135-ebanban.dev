8b410c198abcdbde6945b87005354d954b0d1462
‘Evan <puzonevan@gmail.com>
2022-04-20 13:24:40 -0700

    php hello world

---
+++ cgi-bin/php/hello-html-world.php
@@ -0,0 +1,24 @@
+<?php
+    header("Cache-control: no-cache");
+    header("Content-type: text/html");
+?>
+
+<html>
+    <head>
+        <title>Hello, PHP!</titl>
+    </head>
+
+    <body>
+        <h1><?php echo "Evan Puzon was here - Hello, PHP!"?></h1>
+        <?php print("<p>This page was generated with the PHP programming language")?>
+
+        <?php 
+            $date = getdate();
+            $formatted = $date['weekday'] + $date['month'];
+
+            print("<p>Current Time: $formatted</p>");
+        ?>
+
+    </body>
+
+</html>
