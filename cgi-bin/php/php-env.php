5596ce894cf46f062e9a0ff47d4d0d3d48550122
‘Evan <puzonevan@gmail.com>
2022-04-20 14:30:12 -0700

    php env

---
+++ cgi-bin/php/php-env.php
@@ -0,0 +1,25 @@
+<?php
+    header("Cache-control: no-cache");
+    header("Content-type: text/html");
+?>
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Environment Variables</title>
+    </head>
+
+    <body>
+        <h1 align="center">Environment Variables</h1>
+        <?php
+            $env = $_ENV;
+
+            foreach($env as $key => $value){
+                print("<b>$key</b> = $value <br />");
+            }
+
+        ?>
+    </body>
+
+</html>
