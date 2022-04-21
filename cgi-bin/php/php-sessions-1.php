ddb37ce40abfe2a6a5c40aab3ac8172859c84158
‘Evan <puzonevan@gmail.com>
2022-04-21 10:59:51 -0700

    php session 1

--- cgi-bin/php/php-sessions-1.php
+++ cgi-bin/php/php-sessions-1.php
@@ -21,10 +21,13 @@
     <body>
         <h1 align="center">PHP Sessions Page 1</h1>
 
-        <p><b><?php if($_SESSION["username"]){ echo $_SESSION["username"]; }else{ echo "mysterio"; }?></b></p>
-        <?php
-            
-        ?>
+        <p><b>Name: </b><?php if($_SESSION["username"]){ echo $_SESSION["username"]; }else{ echo "mysterio"; }?></p>
+        
+        <p><a href="/cgi-bin/php/php-sessions-2.php">Session Page 2</a></p>
+        <p><a href="/index.html">Home</a></p>
+        <form action="/cgi-bin/php/php-destroy-session.php" method="GET">
+            <button type="submit">Destroy Session</button>
+        </form>
     </body>
 
 </html>
