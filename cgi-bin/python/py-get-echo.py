0e73ddc5cab96e32b77e9c722d7ed096a2691f75
‘Evan <puzonevan@gmail.com>
2022-04-21 15:02:29 -0700

    get echo python

--- cgi-bin/python/py-get-echo.py
+++ cgi-bin/python/py-get-echo.py
@@ -6,9 +6,10 @@ print("Cache-Control:no-cache")
 print("Content-Type:text/html\r\n\r\n")
 
 print("<html>")
-print("<head><title>Environment Python</title></head>")
+print("<head><title>Get Request Echo</title></head>")
 print("<body>")
 
+print("<h1>Get Request Echo</h1>")
 query = os.environ["QUERY_STRING"]
 formatted = urllib.parse.parse_qs(query)
 print("<p>Raw Query String: ", query ,"</p>")
