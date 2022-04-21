501b6d4d756f8a2c72346dc28b3ca397a88c1efa
‘Evan <puzonevan@gmail.com>
2022-04-21 14:28:17 -0700

    hello world python

--- cgi-bin/python/hello-html-world.py
+++ cgi-bin/python/hello-html-world.py
@@ -2,21 +2,21 @@
 # from datetime import date
 # import socket
 
-print("Cache-Control: no-cache")
-print("Content-type: text/html")
+# print("Cache-Control: no-cache")
+# print("Content-type: text/html")
 
-print("<html>")
-print("<head><title>Hello CGI World</title></head>")
-print("<body>")
+# print("<html>")
+# print("<head><title>Hello CGI World</title></head>")
+# print("<body>")
 
-print("<h1>Evan Puzon - Hello Python!</h1>")
-print("<p>This page was generated with the python programming language</p>")
-# today = date.today()
-# print("<p>Current Date: ", today.strftime("%B %d, %Y"), "</p>")
-# hostname = socket.gethostname()
-# ip = socket.gethostbyname(hostname)
-# print("<p>Your IP Address: ", ip, "</p>")
+# print("<h1>Evan Puzon - Hello Python!</h1>")
+# print("<p>This page was generated with the python programming language</p>")
+# # today = date.today()
+# # print("<p>Current Date: ", today.strftime("%B %d, %Y"), "</p>")
+# # hostname = socket.gethostname()
+# # ip = socket.gethostbyname(hostname)
+# # print("<p>Your IP Address: ", ip, "</p>")
 
-print("</body></html>")
+# print("</body></html>")
 
 
