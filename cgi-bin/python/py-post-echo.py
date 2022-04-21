f0c3c87e46f98a0904ac2adb6080415df301cfd8
‘Evan <puzonevan@gmail.com>
2022-04-21 15:14:17 -0700

    get echo python

---
+++ cgi-bin/python/py-post-echo.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import cgi, cgitb 
+
+print("Cache-Control:no-cache")
+print("Content-Type:text/html\r\n\r\n")
+
+print("<html>")
+print("<head><title>Post Request Echo</title></head>")
+print("<body>")
+
+print("<h1>Post Request Echo</h1>")
+
+data = cgi.FieldStorage()
+
+print("<p>Message Body</p>")
+print("<ul>")
+
+for key in data.keys():
+    print("<li>", key, " = ", data[key], "</li>")
+
+print("</ul>")
+
+
+print("</body></html>")
