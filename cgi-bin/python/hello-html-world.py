26354ceea7b6c09ba3d41dfcb4ecceb95b45bb32
‘Evan <puzonevan@gmail.com>
2022-04-21 14:27:47 -0700

    hello world python

--- cgi-bin/python/hello-html-world.py
+++ cgi-bin/python/hello-html-world.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-from datetime import date
-import socket
+# from datetime import date
+# import socket
 
 print("Cache-Control: no-cache")
 print("Content-type: text/html")
@@ -11,11 +11,11 @@ print("<body>")
 
 print("<h1>Evan Puzon - Hello Python!</h1>")
 print("<p>This page was generated with the python programming language</p>")
-today = date.today()
-print("<p>Current Date: ", today.strftime("%B %d, %Y"), "</p>")
-hostname = socket.gethostname()
-ip = socket.gethostbyname(hostname)
-print("<p>Your IP Address: ", ip, "</p>")
+# today = date.today()
+# print("<p>Current Date: ", today.strftime("%B %d, %Y"), "</p>")
+# hostname = socket.gethostname()
+# ip = socket.gethostbyname(hostname)
+# print("<p>Your IP Address: ", ip, "</p>")
 
 print("</body></html>")
 
