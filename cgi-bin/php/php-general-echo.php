32e508fa1449fb2b5e183ef17861362a7bf7f492
‘Evan <puzonevan@gmail.com>
2022-04-21 10:41:19 -0700

    php post echo

--- cgi-bin/php/php-general-echo.php
+++ cgi-bin/php/php-general-echo.php
@@ -13,7 +13,7 @@
 
     <body>
         <h1 align="center">General Request Echo</h1>
-        <p><b>HTTP Protocol: </b><?php echo $_ENV["SERVER_PROTOCOL"]?></p>
+        <p><b>HTTP Protocol: </b><?php print($_ENV["SERVER_PROTOCOL"])?></p>
         <p><b>HTTP Method: </b><?php echo $_ENV["REQUEST_METHOD"]?></p>
         <p><b>Query String: </b><?php echo $_ENV["QUERY_STRING"]?></p>
         <?php
