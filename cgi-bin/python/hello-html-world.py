d6c79cdd5a4e5495ca6bd9a9430e222f4c1d074d
‘Evan <puzonevan@gmail.com>
2022-04-21 12:09:22 -0700

    change permissions python files

--- cgi-bin/python/hello-html-world.py
+++ cgi-bin/python/hello-html-world.py
@@ -1,5 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+import cgitb
 
-print("Cache-Control: no-cache")
-print("Content-type: text/html")
-print("<html></html>")
+cgitb.enable()
+
+print("Content-Type: text/html;charset=utf-8")
+print ("Content-type:text/html\r\n")
+print("<H1> Hello, From python server :) </H1>")
