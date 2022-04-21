d5532e5be7b355d9b396b538dda1a4750aa154a7
‘Evan <puzonevan@gmail.com>
2022-04-21 11:55:12 -0700

    change permissions python files

--- cgi-bin/python/hello-html-world.py
+++ cgi-bin/python/hello-html-world.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python
 
-print("Cache-Control: no-cache")
-print("Content-type: text/html")
+
+print "Content-type:text/html\r\n\r\n"
+print '<html>'
+print '<head>'
+print '<title>Hello World - First CGI Program</title>'
+print '</head>'
+print '<body>'
+print '<h2>Hello World! This is my first CGI program</h2>'
+print '</body>'
+print '</html>'
