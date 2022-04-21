ecc135a687d0462f8e56c455bb8074edfd317507
‘Evan <puzonevan@gmail.com>
2022-04-21 10:03:54 -0700

    fix C sessions

--- cgi-bin/C/c-destroy-session.c
+++ cgi-bin/C/c-destroy-session.c
@@ -15,11 +15,11 @@ int main(int argc, char **argv, char **envp)
   printf("<h1>C Session Destroyed</h1>");
 
   // Links
-  printf("<a href=\"/cgi-bin/c-sessions-1.cgi\">Back to Page 1</a>");
+  printf("<a href=\"/cgi-bin/C/c-sessions-1.cgi\">Back to Page 1</a>");
   printf("<br />");
-  printf("<a href=\"/cgi-bin/c-sessions-2.cgi\">Back to Page 2</a>");
+  printf("<a href=\"/cgi-bin/C/c-sessions-2.cgi\">Back to Page 2</a>");
   printf("<br />");
-  printf("<a href=\"/c-cgiform.html\">C CGI Form</a>");
+  printf("<a href=\"/index.html\">C CGI Form</a>");
 
   printf("</body>");
   printf("</html>");
