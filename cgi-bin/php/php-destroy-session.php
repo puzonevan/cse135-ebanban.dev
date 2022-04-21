768fe349cc7743a6fb4b9f3cfce74189b3c549c8
‘Evan <puzonevan@gmail.com>
2022-04-21 11:34:32 -0700

    php delete session

--- cgi-bin/php/php-destroy-session.php
+++ cgi-bin/php/php-destroy-session.php
@@ -1,9 +1,6 @@
 <?php
     header("Cache-control: no-cache");
     header("Content-type: text/html");
-
-    $_SESSION = array();
-    if (session_status() == PHP_SESSION_ACTIVE) { session_destroy(); }
 ?>
 
 
@@ -21,7 +18,11 @@
         <p><a href="/cgi-bin/php/php-sessions-1.php">Session Page 1</a></p>
         <p><a href="/cgi-bin/php/php-sessions-2.php">Session Page 2</a></p>
         <p><a href="/index.html">Home</a></p>
-        
+        <?php 
+
+            session_unset();
+            session_destroy();
+        ?>
     </body>
 
 </html>
