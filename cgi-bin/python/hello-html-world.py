edbeba5879ffa217148a079eecf4f52ebf01c869
‘Evan <puzonevan@gmail.com>
2022-04-21 14:30:43 -0700

    hello world python

--- cgi-bin/python/hello-html-world.py
+++ cgi-bin/python/hello-html-world.py
@@ -20,7 +20,7 @@
 # print("</body></html>")
 
 print("Content-Type: text/html;charset=utf-8")
-# print ("Content-type:text/html\r\n")
+print ("Content-type:text/html\r\n")
 # print("<H1> Hello, From python server :) </H1>")
 
 
