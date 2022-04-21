7bfff6a9c028a5db5a10010d3d5d0a3d47d741b8
‘Evan <puzonevan@gmail.com>
2022-04-21 10:25:15 -0700

    fix C sessions

---
+++ cgi-bin/php/php-post-echo.php
@@ -0,0 +1,29 @@
+<?php
+    header("Cache-control: no-cache");
+    header("Content-type: text/html");
+?>
+
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>POST Request Echo</title>
+    </head>
+
+    <body>
+        <h1 align="center">POST Request Echo</h1>
+        <?php
+            $data = htmlspecialchars($_POST);
+
+            print("<p>Message Body</p>");
+
+            print("<ul>");
+            foreach($data as $key => $value){
+                print("<li>$key = $value</li>");
+            }
+            print("</ul>");
+        ?>
+    </body>
+
+</html>
