a9e40ba4b9a90fa3b524962349e1aafaff5daade
‘Evan <puzonevan@gmail.com>
2022-04-22 21:31:29 -0700

    python cookie setup

--- cgi-bin/php/php-destroy-session.php
+++ cgi-bin/php/php-destroy-session.php
@@ -25,7 +25,7 @@
 </html>
 
 <?php 
-    setcookie('username', '', time() - 3600, '/');
+    setcookie('username', '', time() - 3600, '/cgi-bin/php/php-destroy-session.php');
     unset($_COOKIE['username']);
     # https://stackoverflow.com/questions/3989347/php-why-cant-i-get-rid-of-this-session-id-cookie
     $params = session_get_cookie_params();
