72a52c46d62f1ac26627beacd29359d740bd6f86
‘Evan <puzonevan@gmail.com>
2022-04-22 20:41:03 -0700

    python cookie setup

--- cgi-bin/python/py-sessions-1.py
+++ cgi-bin/python/py-sessions-1.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
-from os import environ
-import cgi, cgitb 
+# from os import environ
+# import cgi, cgitb 
 
-data = cgi.FieldStorage()
-cookies = {}
-for key in data.keys():
-    cookies[key] = data[key].value;
+# data = cgi.FieldStorage()
+# cookies = {}
+# for key in data.keys():
+#     cookies[key] = data[key].value;
 
 username = "mysterio"
-# https://www.tutorialspoint.com/python/python_cgi_programming.htm
-if environ.has_key('HTTP_COOKIE'):
-   for cookie in map(environ['HTTP_COOKIE'].split(';').strip()):
-        (key, value) = cookie.split('=')
-        if key == "username":
-            username = value
-elif "username" in cookies.keys():
-    username = cookies["username"]
+# # https://www.tutorialspoint.com/python/python_cgi_programming.htm
+# if environ.has_key('HTTP_COOKIE'):
+#    for cookie in map(environ['HTTP_COOKIE'].split(';').strip()):
+#         (key, value) = cookie.split('=')
+#         if key == "username":
+#             username = value
+# elif "username" in cookies.keys():
+#     username = cookies["username"]
 
 
 print("Cache-Control:no-cache")
