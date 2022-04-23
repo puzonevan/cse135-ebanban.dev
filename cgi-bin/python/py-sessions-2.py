b4238de16d94d2ba20590bf9cef4fa84d6da2069
‘Evan <puzonevan@gmail.com>
2022-04-22 21:41:35 -0700

    python session 2 setup

---
+++ cgi-bin/python/py-sessions-2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+from os import environ
+import cgi, cgitb 
+
+
+username = "mysterio"
+if "HTTP_COOKIE" in environ.keys(): 
+    # for cookie in map(environ['HTTP_COOKIE'].split(';').strip()):
+    #     (key, value) = cookie.split('=')
+    #     if key == "username":
+    #         username = value
+    username = environ['HTTP_COOKIE']
+
+print("Set-Cookie:", username)
+print("Cache-Control:no-cache")
+print("Content-Type:text/html\r\n\r\n")
+
+print("<html>")
+print("<head><title>Python Sessions</title></head>")
+print("<body>")
+
+print("<h1>Python Sessions Page 2</h1>")
+
+print("<p><b>Name: </b>", username, "</p>")
+
+print("<p><a href='/cgi-bin/python/py-sessions-1.py'>Session Page 1</a></p>")
+print("<p><a href='/index.html'>Home</a></p>")
+print("<form action='/cgi-bin/python/py-destroy-session.py' method='GET'>")
+print("<button type='submit'>Destroy Session</button>")
+print("</form>")
+
+
+print("</body></html>")
