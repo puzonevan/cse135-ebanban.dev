25cbf58dae0f84d7c56eb6ef766aad30073ffcbe
‘Evan <puzonevan@gmail.com>
2022-04-22 21:16:59 -0700

    python cookie setup

--- cgi-bin/php/php-destroy-session.php
+++ cgi-bin/php/php-destroy-session.php
@@ -19,11 +19,14 @@
         <p><a href="/cgi-bin/php/php-sessions-1.php">Session Page 1</a></p>
         <p><a href="/cgi-bin/php/php-sessions-2.php">Session Page 2</a></p>
         <p><a href="/index.html">Home</a></p>
-        <?php 
-            set_cookie("username", "", time() - 3600, "/");
-            session_unset();
-            session_destroy();
-        ?>
+        
     </body>
 
 </html>
+
+<?php 
+    unset($_COOKIE['username']);
+    set_cookie("username", "", time() - 3600, "/");
+    session_unset();
+    session_destroy();
+?>
