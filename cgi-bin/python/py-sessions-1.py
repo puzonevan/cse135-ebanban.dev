cb8d1f8b83f8cd248769e0692e1453de449b5df7
‘Evan <puzonevan@gmail.com>
2022-04-27 13:03:31 -0700

    cookie python

--- cgi-bin/python/py-sessions-1.py
+++ cgi-bin/python/py-sessions-1.py
@@ -10,12 +10,11 @@ for key in data.keys():
 username = "mysterio"
 if "username" in cookies.keys(): 
     username = cookies["username"]
-# elif "HTTP_COOKIE" in environ.keys(): 
-#     # for cookie in map(environ['HTTP_COOKIE'].split(';').strip()):
-#     #     (key, value) = cookie.split('=')
-#     #     if key == "username":
-#     #         username = value
-#     username = environ['HTTP_COOKIE']
+elif "HTTP_COOKIE" in environ.keys(): 
+    for cookie in map(environ['HTTP_COOKIE'].split(';').strip()):
+        (key, value) = cookie.split('=')
+        if key == "username":
+            username = value
 
 # # https://www.tutorialspoint.com/python/python_cgi_programming.htm
 # if environ.has_key('HTTP_COOKIE'):
