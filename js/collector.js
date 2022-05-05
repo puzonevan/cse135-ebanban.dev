6529904aa098d4edaac9c2e5c61c2130900852c8
‘Evan <puzonevan@gmail.com>
2022-05-05 14:24:01 -0700

    load times for performance

--- js/collector.js
+++ js/collector.js
@@ -9,10 +9,26 @@ const collect = () => {
     let static = {
         "user-agent": navigator.userAgent, 
         "user-language": navigator.language, 
-        "cookies-enabled": navigator.cookieEnabled
+        "cookies-enabled": navigator.cookieEnabled, 
+        "javascript-enabled": true, 
+        "images-enabled": true, 
+        "css-enabled": true, 
+        "screen-dimension": `${window.screen.width}x${window.screen.height}`,
+        "window-dimension": `${window.screen.width}x${window.screen.height}`,
+        "network-connection": navigator.connection
+    }
+
+    let performance = {
+        "load-time": {
+            "time": window.performance,
+            "start": window.performance.timing.domContentLoadedEventStart, 
+            "end": window.performance.timing.domContentLoadedEventEnd, 
+            "total": window.performance.timing.domContentLoadedEventEnd - window.performance.timing.domContentLoadedEventStart
+        }
     }
 
     console.log(static);
+    console.log(performance);
 }
 
 window.onload = collect();
